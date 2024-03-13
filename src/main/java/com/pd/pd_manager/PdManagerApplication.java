6b94cc2eecc59d76e8edaa395703979cd6d8af5f
KA-7A <galagan.ka@phystech.edu>
2024-03-13 22:18:15 +0300

    Initial commit

---
+++ src/main/java/com/pd/pd_manager/PdManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pd.pd_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pd/pd_manager/PdManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.pd_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
