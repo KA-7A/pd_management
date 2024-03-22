6effddf57b1af6a2ffcb08d6c02b92a8dfdd28e9
KA-7A <galagan.ka@phystech.edu>
2024-03-22 16:40:08 +0300

    Сущности в Person теперь идут подряд.

--- src/main/java/com/pd/pd_manager/model/Person.java
+++ src/main/java/com/pd/pd_manager/model/Person.java
@@ -11,7 +11,13 @@ import java.util.Date;
 public class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "s_pd_person")
+    @SequenceGenerator(name = "s_pd_person",
+                       sequenceName = "s_pd_person",
+                       initialValue = 1, allocationSize = 1)    // Да, не очень производительно, но оно и не надо.
+                                                    // Не слишком безопасно, но пока сойдёт
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id_person")
     private long id_person;
 
     private String v_name, v_last_name, v_patronymic_name;
